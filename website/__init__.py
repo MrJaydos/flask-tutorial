c79c61a51460ca4beaf3f78c26892dd3f0562f86
Jayden Dickinson <jaydend93@gmail.com>
2026-04-28 10:35:31 +1000

    Register the blueprints

--- website/__init__.py
+++ website/__init__.py
@@ -5,4 +5,10 @@ def create_app():
     # Initialised secret key
     app.config['SECRET_KEY'] = 'SECRETKEY'
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
