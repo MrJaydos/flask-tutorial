746bbc0349117aefb1e20f5390bef0de9bd0ae64
Jayden Dickinson <jaydend93@gmail.com>
2026-04-28 20:18:18 +1000

    Added notes explaining index.js

--- website/static/index.js
+++ website/static/index.js
@@ -1,8 +1,12 @@
+// Function for deleteing a note. Its called from the home.html file.
+
 function deleteNote(noteId) {
+    // passes in the note id from the item the button was clicked on
     fetch('/delete-note', {
         method: 'POST',
         body: JSON.stringify({noteId: noteId})
     }).then((_res) => {
+        // refreshes the page so the note disappears when the button is clicked on
         window.location.href = '/';
     });
 }
