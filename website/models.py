78782050be23d2e5973fc3ace78d745947b6a664
Jayden Dickinson <jaydend93@gmail.com>
2026-04-28 20:10:25 +1000

    Finished will add comments to explain code for myself to refrence back on

--- website/models.py
+++ website/models.py
@@ -1,7 +1,6 @@
 from . import db
 from flask_login import UserMixin
-from sqlalchemy.sql import func
-
+from sqlalchemy import func
 
 
 # Define the schema of how the database shoud look within this classes
