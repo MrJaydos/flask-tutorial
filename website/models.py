8ed4f6dbc10d2d67e96d2fc7efbcc05f2b8b77a1
Jayden Dickinson <jaydend93@gmail.com>
2026-04-28 18:56:41 +1000

    ?

---
+++ website/models.py
@@ -0,0 +1,24 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+
+# Define the schema of how the database shoud look within this classes
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    # String must have a length and unique means only one person can have an email
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
+
+    
